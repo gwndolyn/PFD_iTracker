e0965f3f5e7abd52bb77484a84bb6d49158026fa
Gwendolyn <gwenxdolyn@gmail.com>
2023-02-02 15:58:50 +0800

    done

--- app/src/main/java/sg/edu/np/mad/dontslack/Contact.java
+++ app/src/main/java/sg/edu/np/mad/dontslack/Contact.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Contact extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    private static  User user;
+    private static final String KEY_USERNAME = "userName";
+    DBHandler dbHandler = new DBHandler(this,null,null,1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,13 @@ public class Contact extends AppCompatActivity {
         assert actionBar != null;
         actionBar.hide();
 
-        //Back home page button
+      //  String Username = sharedPreferences.getString(KEY_USERNAME, "");
+      //  if(!Username.equals("")) {
+       //     user = dbHandler.findUser(Username);
+        //    TextView profileUsername = findViewById(R.id.textAccessCode);
+       //     profileUsername.setText(user.getUsername());
+      //  }
+            //Back home page button
         ImageView backHomePage = findViewById(R.id.backHome6);
         backHomePage.setOnClickListener(v -> {
             Intent myIntent = new Intent(Contact.this, HomePage.class);
